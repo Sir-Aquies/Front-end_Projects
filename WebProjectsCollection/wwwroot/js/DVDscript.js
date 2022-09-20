9e6e4cdcb7e3761cea1ac54a098f127c11772d97
JECT <eliank500@gmail.com>
2022-09-20 15:06:02 -0500

    dvd logo now is created when the user clicks the screen, also added a bounce property when the dvd logo collide with each other. The bounce property works fine. I got some bugs as i was triying to figuring out how to make the property work, I was able to fix them the best i could but ... can't really say it works perfect.

--- WebProjectsCollection/wwwroot/js/DVDscript.js
+++ WebProjectsCollection/wwwroot/js/DVDscript.js
@@ -1,58 +1,140 @@
 ﻿var id = 0;
+const divArray = [];
 
-function CreateDVD() {
-    const div = document.createElement("div");
-    div.className = "mask1 RainbowDVD";
-    div.id = `Div${id++}`;
-    document.body.appendChild(div);
+document.addEventListener("click", function (e) {
+	CreateDVD(e.clientX, e.clientY);
+});
 
-    //Ramdomize the initial direction
-    var dxRng = Math.floor(Math.random() * 2);
-    var dyRng = Math.floor(Math.random() * 2);
+function CreateDVD(posx, posy) {
+	const div = document.createElement("div");
+	div.className = "mask1 RainbowDVD";
+	div.id = `Div${id++}`;
+	div.lastCollide = null;
+	document.body.appendChild(div);
+	divArray.push(div);
+	vx = 2;
+	vy = 2;
 
-    if (dxRng == 0) {
-        div.dx = -1;
-    }
-    else {
-        div.dx = 1;
-    }
-    if (dyRng == 0) {
-        div.dy = -1;
-    }
-    else {
-        div.dy = 1;
-    }
+	div.addEventListener("click", function () {
+		event.stopPropagation();
+    });
 
-    //Ramdomize the initial position
-    let xRng = Math.floor(Math.random() * (screen.width - 200));
-    let yRng = Math.floor(Math.random() * (screen.height - 200));
-    div.style.left = `${xRng}px`;
-    div.style.top = `${yRng}px`;
+	//Ramdomize the initial direction
+	var dxRng = Math.floor(Math.random() * 2);
+	var dyRng = Math.floor(Math.random() * 2);
 
-    //The function that makes the div bounce
-    div.bounce = setInterval(function () {
-        let x = parseInt(div.style.left);
-        x += div.dx;
-        div.style.left = `${x}px`;
+	if (dxRng == 0) {
+		div.dx = -vx;
+	}
+	else {
+		div.dx = vx;
+	}
+	if (dyRng == 0) {
+		div.dy = -vy;
+	}
+	else {
+		div.dy = vy;
+	}
 
-        let y = parseInt(div.style.top);
-        y += div.dy;
-        div.style.top = `${y}px`;
+	//Set the initial position base on the mouse click position
+	div.style.left = `${ parseInt(posx) - (div.clientWidth / 2) }px`;
+	div.style.top = `${ parseInt(posy) - (div.clientHeight / 2) }px`;
 
-        if (x >= (window.innerWidth - div.clientWidth)) {
-            div.dx = -1;
-        }
+	//Ramdomize the initial position
+	//let xRng = Math.floor(Math.random() * (screen.width - 200));
+	//let yRng = Math.floor(Math.random() * (screen.height - 200));
+	//div.style.left = `${xRng}px`;
+	//div.style.top = `${yRng}px`;
 
-        if (x <= 0) {
-            div.dx = 1;
-        }
+	//The function that makes the div bounce
+	div.bounce = setInterval(function () {
+		for (const x of divArray) {
+			if (div !== x && div.lastCollide != x) {
+				if (Collide(div, x)) {
+					if ((div.dy !== x.dy) && div.dx === x.dx) {
+						div.dy = -div.dy;
+						x.dy = -x.dy;
 
-        if (y >= (window.innerHeight - div.clientHeight)) {
-            div.dy = -1;
-        }
+						div.lastCollide = x;
+						continue;
+					}
 
-        if (y <= 0) {
-            div.dy = 1;
-        }
-    }, 1);
+					if ((div.dy !== x.dy) && div.dx !== x.dx) {
+						div.dy = -div.dy;
+						x.dy = -x.dy;
+						div.dx = -div.dx;
+						x.dx = -x.dx;
+
+						div.lastCollide = x;
+						continue;
+					}
+
+					if ((div.dy === x.dy) && div.dx === x.dx) {
+						div.dx = -div.dx;
+						x.dy = -x.dy;
+						x.dx = -x.dx;
+
+						div.lastCollide = x;
+						continue;
+					}
+
+					if ((div.dy === x.dy) && div.dx !== x.dx) {
+						div.dx = -div.dx;
+						x.dx = -x.dx;
+
+						div.lastCollide = x;
+						continue;
+					}
+				}
+			}
+		}
+		div.lastCollide = null;
+
+		let x = parseInt(div.style.left);
+		x += div.dx;
+		div.style.left = `${x}px`;
+
+		let y = parseInt(div.style.top);
+		y += div.dy;
+		div.style.top = `${y}px`;
+
+		if (x >= (window.innerWidth - div.clientWidth)) {
+			div.dx = -div.dx;
+		}
+
+		if (x <= 0) {
+			div.dx = -div.dx;
+		}
+
+		if (y >= (window.innerHeight - div.clientHeight)) {
+			div.dy = -div.dy;
+		}
+
+		if (y <= 0) {
+			div.dy = -div.dy;
+		}
+
+	}, 10);
+};
+
+//if ((div.offsetLeft + div.offsetWidth) > x.offsetLeft || div.offsetLeft < (x.offsetLeft + x.offsetWidth)) {
+//	div.dx = -div.dx;
+//	div.lastCollide = x;
+//}
+
+//if ((div.offsetTop + div.offsetHeight) > x.offsetTop || div.offsetTop < (x.offsetTop + x.offsetHeight)) {
+//	div.dy = -div.dy;
+//	div.lastCollide = x;
+//}
+
+function Collide(el1, el2) {
+	el1.offsetBottom = el1.offsetTop + el1.offsetHeight;
+	el1.offsetRight = el1.offsetLeft + el1.offsetWidth;
+	el2.offsetBottom = el2.offsetTop + el2.offsetHeight;
+	el2.offsetRight = el2.offsetLeft + el2.offsetWidth;
+
+	return !((el1.offsetBottom < el2.offsetTop) ||
+		(el1.offsetTop > el2.offsetBottom) ||
+		(el1.offsetRight < el2.offsetLeft) ||
+		(el1.offsetLeft > el2.offsetRight))
 };
