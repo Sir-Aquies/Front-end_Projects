826adc7206eb0fc65e5126797ae4046de4b3ea41
JECT <eliank500@gmail.com>
2022-10-16 17:44:26 -0500

    Gravity working great. there another bug when you place a div on top of another, will fix that other day.

--- WebProjectsCollection/wwwroot/js/PhysicsScript.js
+++ WebProjectsCollection/wwwroot/js/PhysicsScript.js
@@ -1,12 +1,13 @@
 const elms = new Set();
 var id = 0;
+
 document.addEventListener("mousedown", function (e) {
 	const div = DrawDiv(e);
 
 	document.onmouseup = function () {
 		div.gravity(div);
-		document.onmousemove = null;
-		document.onmouseup = null;
+		document.onmousemove = function () { };
+		document.onmouseup = function () { };
 	};
 
 	document.onmousemove = function (e) {
@@ -22,23 +23,40 @@ function DrawDiv(e) {
 	div.style.top = e.clientY + "px";
 	div.Xi = e.clientX;
 	div.Yi = e.clientY;
+	div.falling = false;
+	div.grounded = false;
 
-	div.gravity = function () {
-		Gravity(div);
+	div.gravity = function (div, ignore) {
+		Gravity(div, ignore);
 	};
 
-	div.onmouseover = function () {
-		this.style.cursor = "grab";
-	};
+	div.ondragstart = event.preventDefault();
 
 	div.onmousedown = function (e) {
 		event.stopPropagation();
-		clearInterval(div.stop);
+
+		if (this.stop) {
+			clearInterval(this.stop);
+		}
+		
 		this.style.cursor = "grabbing";
 		this.grabX = this.offsetLeft - e.clientX;
 		this.grabY = this.offsetTop - e.clientY;
 
+		for (const div2 of elms) {
+			if (!div2.grounded) {
+				div2.falling = true;
+			}
+		}
+
+		for (const div2 of elms) {
+			if (this.id !== div2.id) {
+				div2.gravity(div2, this);
+			}
+		}
+
 		document.onmousemove = function (e) {
+			div.Vtimer = setInterval();
 			div.style.left = (div.grabX + e.clientX) + "px";
 			div.style.top = (div.grabY + e.clientY) + "px";
 		};
@@ -46,19 +64,13 @@ function DrawDiv(e) {
 
 	div.onmouseup = function () {
 		this.style.cursor = "grab";
-		document.onmousemove = null;
-	}
+		document.onmousemove = function () { };
+		Throwing(this);
 
-	div.onmouseup = function () {
 		for (const div2 of elms) {
 			div2.gravity(div2);
 		}
-		document.onmousemove = null;
-	};
-
-	div.ondragstart = function (event) {
-		event.preventDefault();
-	};
+	}
 
 	div.ondblclick = function () {
 		elms.delete(div);
@@ -71,37 +83,43 @@ function DrawDiv(e) {
 	return div;
 }
 
-function Gravity(div) {
-	const g = 10;
+function Throwing(div) {
 
-	div.stop = setInterval(function() {
+}
+
+function Gravity(div, ignoreDiv) {
+	const g = 9;
+
+	div.stop = setInterval(function () {
 		div.style.top = (div.offsetTop + g) + "px";
+		div.falling = true;
 
 		for (const div2 of elms) {
-			if (Collide(div, div2) && div.id !== div2.id) {
-				div.style.top = (div2.offsetTop - div.offsetHeight) + "px";
-				clearInterval(div.stop);
+			if (ignoreDiv) {
+				if (ignoreDiv.id === div2.id) {
+					continue;
+				}
+			}
+
+			if (div.id !== div2.id) {
+				if (Collide(div, div2)) {
+					if (!div2.falling) {
+						div.style.top = (div2.offsetTop - div.offsetHeight - 1) + "px";
+						clearInterval(div.stop);
+						div.falling = false;
+					}
+				}
 			}
 		}
 
 		if (div.offsetTop + div.offsetHeight > window.innerHeight) {
 			div.style.top = (window.innerHeight - div.offsetHeight) + "px";
 			clearInterval(div.stop);
+			div.falling = false;
+			div.grounded = true;
 		}
 	}, 10)
 }
-
-function Collide(el1, el2) {
-	el1.offsetBottom = el1.offsetTop + el1.offsetHeight;
-	el1.offsetRight = el1.offsetLeft + el1.offsetWidth;
-	el2.offsetBottom = el2.offsetTop + el2.offsetHeight;
-	el2.offsetRight = el2.offsetLeft + el2.offsetWidth;
-
-	return !((el1.offsetBottom < el2.offsetTop) ||
-		(el1.offsetTop > el2.offsetBottom) ||
-		(el1.offsetRight < el2.offsetLeft) ||
-		(el1.offsetLeft > el2.offsetRight))
-};
 
 function SizeDiv(e, div) {
 	let x = parseInt(div.style.left);
@@ -150,3 +168,15 @@ function SizeDiv(e, div) {
 	div.style.width = dx + "px";
 	div.style.height = dy + "px";
 }
+
+function Collide(el1, el2) {
+	el1.offsetBottom = el1.offsetTop + el1.offsetHeight;
+	el1.offsetRight = el1.offsetLeft + el1.offsetWidth;
+	el2.offsetBottom = el2.offsetTop + el2.offsetHeight;
+	el2.offsetRight = el2.offsetLeft + el2.offsetWidth;
+
+	return !((el1.offsetBottom < el2.offsetTop) ||
+		(el1.offsetTop > el2.offsetBottom) ||
+		(el1.offsetRight < el2.offsetLeft) ||
+		(el1.offsetLeft > el2.offsetRight))
+};
